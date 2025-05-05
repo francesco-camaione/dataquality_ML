5f860497085f31b2208fff1d36540459eab1d132
Francesco Camaione <francesco.camaione@gmail.com>
2025-05-05 19:17:05 +0200

    dev anomaly_table to identify anomalies

--- app/main.py
+++ app/main.py
@@ -18,20 +18,40 @@ spark = (
         "org.apache.hadoop:hadoop-aws:3.3.4,"
         "software.amazon.awssdk:url-connection-client:2.20.160",
     )
-    .config("spark.jars.repositories", "https://repo1.maven.org/maven2/,https://repos.spark-packages.org/,https://repo.maven.apache.org/maven2/")
-    .config("spark.sql.catalog.s3tablescatalog", "org.apache.iceberg.spark.SparkCatalog")
-    .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
+    .config(
+        "spark.jars.repositories",
+        "https://repo1.maven.org/maven2/,https://repos.spark-packages.org/,https://repo.maven.apache.org/maven2/",
+    )
+    .config(
+        "spark.sql.catalog.s3tablescatalog", "org.apache.iceberg.spark.SparkCatalog"
+    )
+    .config(
+        "spark.sql.extensions",
+        "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
+    )
     .config("spark.sql.catalog.s3tablescatalog.glueRegion", AWS_REGION)
     .config("spark.sql.catalog.s3tablescatalog.glue.catalog-id", GLUE_CATALOG_ID)
     .config("spark.sql.catalog.s3tablescatalog.type", "glue")
     .config("spark.sql.catalog.s3tablescatalog.warehouse", S3_WAREHOUSE)
-    .config("spark.sql.catalog.s3tablescatalog.io-impl", "org.apache.iceberg.aws.s3.S3FileIO")
-    .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
+    .config(
+        "spark.sql.catalog.s3tablescatalog.io-impl",
+        "org.apache.iceberg.aws.s3.S3FileIO",
+    )
+    .config(
+        "spark.hadoop.fs.s3a.aws.credentials.provider",
+        "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider",
+    )
     .config("spark.hadoop.fs.s3a.access.key", AWS_ACCESS_KEY)
     .config("spark.hadoop.fs.s3a.secret.key", AWS_SECRET_KEY)
     .config("spark.hadoop.fs.s3a.endpoint", f"s3.{AWS_REGION}.amazonaws.com")
-    .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
-    .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider")
+    .config(
+        "spark.sql.extensions",
+        "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
+    )
+    .config(
+        "spark.hadoop.fs.s3a.aws.credentials.provider",
+        "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider",
+    )
     .getOrCreate()
 )
 
@@ -41,10 +61,12 @@ print("Spark is running...")
 #     CREATE OR REPLACE TABLE s3tablescatalog.dq_db.unpivoted_datacenter_data
 #     USING iceberg
 #     LOCATION 's3a://dq-monitoring-bucket/iceberg/'
-#     AS SELECT * 
+#     AS SELECT *
 #     FROM parquet.`s3a://dq-monitoring-bucket/raw_data/unpivoted_data.parquet`;
 # """).show()
 
-spark.sql("SELECT * FROM s3tablescatalog.dq_db.unpivoted_datacenter_data limit 10;").show()
+spark.sql(
+    "SELECT count(*) as tot FROM s3tablescatalog.dq_db.unpivoted_datacenter_data;"
+).show()
 
 print("Spark is done!!")
